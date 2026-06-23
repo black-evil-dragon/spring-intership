db74be704132d51ad937f2e6b0a94d9a80754bc4
Semyon Golgan <blackevildragon@mail.ru>
2026-06-23 19:01:05 +0300

    Установка базового spring проекта
    https://start.spring.io/

---
+++ toduo/src/main/java/com/golgan/toduo/ToduoApplication.java
@@ -0,0 +1,13 @@
+package com.golgan.toduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToduoApplication.class, args);
+	}
+
+}
---
+++ toduo/src/test/java/com/golgan/toduo/ToduoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golgan.toduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToduoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
