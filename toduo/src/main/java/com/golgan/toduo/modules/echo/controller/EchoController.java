f7e40b27e4b3d746b567248d94d55ae280f7be36
Semyon Golgan <blackevildragon@mail.ru>
2026-06-25 15:40:22 +0300

    [dev] Разработка второго задания

--- toduo/src/main/java/com/golgan/toduo/modules/echo/controller/EchoController.java
+++ toduo/src/main/java/com/golgan/toduo/modules/echo/controller/EchoController.java
@@ -1,7 +1,9 @@
 package com.golgan.toduo.modules.echo.controller;
 
 import com.golgan.toduo.core.services.LoggerService;
+import com.golgan.toduo.core.utils.IDGenerator;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,8 @@ public class EchoController {
     private final LoggerService lowerLogService;
     private final LoggerService upperLogService;
 
+    private final ApplicationContext context;
+
     /// # Практика 1
     /// - Приложение из ДЗ должно реализовать интефейс логирования.
     /// ## Две реализации:
@@ -27,10 +31,14 @@ public class EchoController {
     /// Изучить работу аннотации `@Qualifier`, чтобы разрешить конфлит между двумя релизациями.
     public EchoController(
         @Qualifier("lowerLogService") LoggerService lowerLogService,
-        @Qualifier("upperLogService") LoggerService upperLogService
+        @Qualifier("upperLogService") LoggerService upperLogService,
+        ApplicationContext context
     ) {
         this.lowerLogService = lowerLogService;
         this.upperLogService = upperLogService;
+
+        // Практика 2
+        this.context = context;
     }
 
     @GetMapping("/hello")
@@ -39,5 +47,16 @@ public class EchoController {
         upperLogService.log("Эндпоинт работает! Хорошего дня)");
         lowerLogService.log("Кто-то посетил нашу страницу");
         return "Hello";
+    }
+
+
+    /// # Практика 2
+    ///
+    /// - Написать бин, который при инициализации загружает данные из properties и выводит их.
+    /// - Реализовать prototype‑бин для генерации уникальных идентификаторов.
+    @GetMapping("/generate-id")
+    @ResponseStatus(HttpStatus.OK)
+    public String generateId() {
+        return context.getBean(IDGenerator.class).getUUID() + "-" + context.getBean(IDGenerator.class).getUUID();
     }
 }
