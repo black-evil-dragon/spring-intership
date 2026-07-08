a2c03f71d517f6871bcccc7f20f1f9056605d9cb
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2026-07-08 17:44:31 +0300

    [refactor] Исправление варнингов, явно поставил поледние версии либ,

---
+++ toduo/src/main/java/com/golgan/toduo/core/mappers/EntityMapper.java
@@ -0,0 +1,31 @@
+package com.golgan.toduo.core.mappers;
+
+import org.mapstruct.MappingTarget;
+
+
+import java.util.List;
+
+
+// TODO DELETE COMMENT: Вообще в задумках разбить его еще на мелкие части, но боюсь сделать излишне
+public interface EntityMapper<
+    Entity,
+    CreateDto, UpdateDto,
+    DetailDto, SummaryDto, ListDto
+    > {
+
+    // Update entity from dto
+    void update(UpdateDto dto, @MappingTarget Entity entity);
+
+
+    // From dto to entity
+    Entity toEntity(CreateDto dto);
+
+
+    // From entity to dto
+    DetailDto toDetailDto(Entity entity);
+
+    SummaryDto toSummaryDto(Entity entity);
+
+    ListDto toListDto(Entity entity);
+    List<ListDto> toListDtoCollection(List<Entity> entities);
+}
--- toduo/src/main/java/com/golgan/toduo/modules/tasks/mappers/TaskMapper.java
+++ toduo/src/main/java/com/golgan/toduo/modules/tasks/mappers/TaskMapper.java
@@ -1,31 +1,21 @@
 package com.golgan.toduo.modules.tasks.mappers;
 
 
+import com.golgan.toduo.core.mappers.EntityMapper;
 import com.golgan.toduo.modules.tasks.dto.*;
 import com.golgan.toduo.modules.tasks.models.TaskEntity;
+
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-import java.util.List;
 
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public interface TaskMapper {
-    // Update
-    void update(TaskUpdateDto dto, @MappingTarget TaskEntity entity);
+public interface TaskMapper extends EntityMapper<
+    TaskEntity,
+    TaskCreateDto, TaskUpdateDto,
+    TaskDetailDto, TaskSummaryDto, TaskListDto
+    > {
 
-
-    // From dto
-    TaskEntity toEntity(TaskCreateDto dto);
-
-
-
-    // From entity
-    TaskDetailDto toDetailDto(TaskEntity Task);
-
-    TaskSummaryDto toSummaryDto(TaskEntity Task);
-
-    TaskListDto toListDto(TaskEntity Task);
-    List<TaskListDto> toListDtoCollection(List<TaskEntity> Tasks);
 }
